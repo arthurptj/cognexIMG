ebbfe0d9bd4cbd99653b3bc56f05a2eb5d5c4c99
Arthur Thompson Johnson <athompson-johnson@fhcmi.org>
2025-07-30 14:46:37 -0400

    Add project files.

---
+++ Fhcmi.Automation.CognexWinform.net48/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Fhcmi.Automation.CognexWinform.net48
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Trigger = new System.Windows.Forms.Button();
+            this.InputExposure = new System.Windows.Forms.NumericUpDown();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cogDisplay1 = new Cognex.VisionPro.Display.CogDisplay();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.InputExposure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cogDisplay1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cogDisplay1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 450F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.Trigger, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.InputExposure, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.lbStatus, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btConnect, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(94, 444);
+            this.tableLayoutPanel2.TabIndex = 0;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // Trigger
+            // 
+            this.Trigger.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Trigger.Location = new System.Drawing.Point(3, 153);
+            this.Trigger.Name = "Trigger";
+            this.Trigger.Size = new System.Drawing.Size(88, 288);
+            this.Trigger.TabIndex = 6;
+            this.Trigger.Text = "Trigger";
+            this.Trigger.UseVisualStyleBackColor = true;
+            this.Trigger.Click += new System.EventHandler(this.Trigger_Click);
+            // 
+            // InputExposure
+            // 
+            this.InputExposure.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.InputExposure.Location = new System.Drawing.Point(3, 128);
+            this.InputExposure.Name = "InputExposure";
+            this.InputExposure.Size = new System.Drawing.Size(88, 20);
+            this.InputExposure.TabIndex = 4;
+            this.InputExposure.ValueChanged += new System.EventHandler(this.InputExposure_ValueChanged);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbStatus.Location = new System.Drawing.Point(3, 0);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(88, 50);
+            this.lbStatus.TabIndex = 0;
+            this.lbStatus.Text = "***";
+            this.lbStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btConnect
+            // 
+            this.btConnect.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btConnect.Location = new System.Drawing.Point(3, 53);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(88, 44);
+            this.btConnect.TabIndex = 1;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Brightness";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cogDisplay1
+            // 
+            this.cogDisplay1.ColorMapLowerClipColor = System.Drawing.Color.Black;
+            this.cogDisplay1.ColorMapLowerRoiLimit = 0D;
+            this.cogDisplay1.ColorMapPredefined = Cognex.VisionPro.Display.CogDisplayColorMapPredefinedConstants.None;
+            this.cogDisplay1.ColorMapUpperClipColor = System.Drawing.Color.Black;
+            this.cogDisplay1.ColorMapUpperRoiLimit = 1D;
+            this.cogDisplay1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cogDisplay1.DoubleTapZoomCycleLength = 2;
+            this.cogDisplay1.DoubleTapZoomSensitivity = 2.5D;
+            this.cogDisplay1.Location = new System.Drawing.Point(103, 3);
+            this.cogDisplay1.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
+            this.cogDisplay1.MouseWheelSensitivity = 1D;
+            this.cogDisplay1.Name = "cogDisplay1";
+            this.cogDisplay1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("cogDisplay1.OcxState")));
+            this.cogDisplay1.Size = new System.Drawing.Size(694, 444);
+            this.cogDisplay1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Demo";
+            this.Load += new System.EventHandler(this.Demo_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.InputExposure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cogDisplay1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Button Trigger;
+        private System.Windows.Forms.NumericUpDown InputExposure;
+        private System.Windows.Forms.Label label1;
+        private Cognex.VisionPro.Display.CogDisplay cogDisplay1;
+    }
+}
+
---
+++ Fhcmi.Automation.CognexWinform.net48/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cognex.VisionPro;
+namespace Fhcmi.Automation.CognexWinform.net48
+{
+    public partial class Form1 : Form
+    {
+        CogFrameGrabbers myFrameGrabbers;
+        ICogFrameGrabber myFrameGrabber;
+        ICogAcqFifo myFifo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Demo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void InputBrightness_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitCamera()
+        {
+            myFrameGrabbers = new CogFrameGrabbers();
+            myFrameGrabber = myFrameGrabbers[0];
+
+            CogStringCollection AvailableVideoFormats = myFrameGrabber.AvailableVideoFormats;
+            myFifo = myFrameGrabber.CreateAcqFifo(AvailableVideoFormats[0], CogAcqFifoPixelFormatConstants.Format8Grey,0,false);
+            SetExposure(100);
+        }
+
+        private ICogImage TakePic()
+        {
+            ICogImage image = null;
+            int trignum;
+            image = myFifo.Acquire(out trignum);
+            return image;
+        }
+
+        private void SetExposure(int val)
+        {
+            myFifo.OwnedExposureParams.Exposure = val;
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            InitCamera();
+            lbStatus.Text = myFrameGrabber.Name;
+            InputExposure.Value = (decimal)myFifo.OwnedExposureParams.Exposure;
+        }
+
+        private void Trigger_Click(object sender, EventArgs e)
+        {
+            cogDisplay1.Image = TakePic();
+            cogDisplay1.Fit();
+        }
+
+        private void InputExposure_ValueChanged(object sender, EventArgs e)
+        {
+            SetExposure((int)InputExposure.Value);
+        }
+    }
+}
